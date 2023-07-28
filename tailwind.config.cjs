e4a9d5d6e83e73a5bae01e5279e12662fc85bd34
Vegard Feste <vif@variant.no>
2023-07-28 12:55:50 +0200

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			'display': ['Recoleta'],
+			'body': ['Graphik']
+		},
+		extend: {
+			colors: {
+				'light-purple': '#534DAC'
+			}
+		},
+	},
+	plugins: [],
+}
