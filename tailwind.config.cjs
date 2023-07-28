c226e80fe819647153f0e7be108b9f1d1c9eb2ff
Vegard Feste <vif@variant.no>
2023-07-28 14:35:57 +0200

    added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,13 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			'recoleta': ['Recoleta'],
-			'graphik': ['Graphik']
-		},
+			'recoleta': ["Recoleta", "serif"],
+			'graphik': ['"Graphik Web"', "system-ui", "sans-serif"],
+		  },
 		extend: {
 			colors: {
 				'light-purple': '#534DAC'
-			}
+			},
 		},
 	},
 	plugins: [],
