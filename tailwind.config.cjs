170523f65bfe528f044549a7dd315a39e049c25a
Ida Klemmetsrud Isaksen <iki@variant.no>
2023-08-03 15:15:49 +0200

    scroll-free shortcuts and one nice font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,17 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		fontFamily: {
-			'recoleta': ['Recoleta'],
-			'graphik': ['Graphik']
-		},
 		extend: {
 			colors: {
 				'light-purple': '#534DAC',
 				'dark-purple': '#35316E',
 				'robins-egg-blue': '#03DAC6'
 			},
-
+			fontFamily: {
+				'recoleta': ['Recoleta'],
+			}
 		},
 	},
 	plugins: [],
